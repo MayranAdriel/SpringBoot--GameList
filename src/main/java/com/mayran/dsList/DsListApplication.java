ede5110eb359ded9e4fc63e7babfe03b895b2895
Mayran Adriel <mayranalbuquerque@gmail.com>
2024-11-14 20:44:28 -0300

    entidade game

---
+++ src/main/java/com/mayran/dsList/DsListApplication.java
@@ -0,0 +1,17 @@
+package com.mayran.dsList;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsListApplication.class, args);
+
+		Double teste = new Double(10.0);
+
+    }
+
+}
